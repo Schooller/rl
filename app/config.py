b3d849d3ebcad719fb14dba70b698fffc49de23a
Schooller <37085412+Schooller@users.noreply.github.com>
2021-03-10 14:56:19 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,3 +1,3 @@
-TELEGRAM_TOKEN = '696629284:AAGejq46isCFK3vL2FertLrQzJq-3SMRjYM'
+TELEGRAM_TOKEN = '---'
 MAIN_URL = 'https://telegra.ph/Golosuvannya-za-V%D1%96ce-prezidenta-parlamentu-R%D1%96shelyevskogo-l%D1%96ceyu-2020-roku-Peredviborcha-kampan%D1%96ya-01-14'
 isWork = True
